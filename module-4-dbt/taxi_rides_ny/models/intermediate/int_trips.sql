ee14a9ae01b9cc67d44e9822a59b3e5673314bdf
Stan_Buren <burundu4ok2000@gmail.com>
2026-02-20 10:54:22 +0000

    completed /intermediate models folder (expanded schema.yml, corrected int_trips_unioned.sql, int_trips.sql)

--- module-4-dbt/taxi_rides_ny/models/intermediate/int_trips.sql
+++ module-4-dbt/taxi_rides_ny/models/intermediate/int_trips.sql
@@ -7,5 +7,52 @@ payment_types AS (
 ),
 
 cleaned_and_enriched AS (
-    {{ dbt_utils.generate_surrogate_key(['u.vendor_id', 'u.pickup_datetime', 'u.pickup_location_id', 'u.service_type']) }} AS trip_is
+    SELECT
+        -- Generate unique trip identifier (surrogate key pattern)
+        {{ dbt_utils.generate_surrogate_key(['u.vendor_id', 'u.pickup_datetime', 'u.pickup_location_id', 'u.service_type']) }} AS trip_id,
+
+        -- identifiers
+        u.vendor_id,
+        u.service_type,
+        u.rate_code_id,
+
+        -- Location IDs
+        u.pickup_location_id,
+        u.dropoff_location_id,
+
+        -- Timestamps
+        u.pickup_datetime,
+        u.dropoff_datetime,
+
+        -- Trip details
+        u.store_and_fwd_flag,
+        u.passenger_count,
+        u.trip_distance,
+        u.trip_type,
+
+        -- Payment breakdown
+        u.fare_amount,
+        u.extra,
+        u.mta_tax,
+        u.tip_amount,
+        u.tolls_amount,
+        u.improvement_surcharge,
+        u.congestion_surcharge,
+        u.total_amount,
+
+        -- Enrich with payment type description
+        COALESCE(u.payment_type, 0) AS payment_type,
+        COALESCE(pt.description, 'Unknown') AS payment_type_description
+
+    FROM unioned u
+    LEFT JOIN payment_types pt
+        ON COALESCE(u.payment_type, 0) = pt.payment_type
 )
+
+SELECT * FROM cleaned_and_enriched
+
+-- Deduplicate: if multiple trips match (same vendor, second, location, service), keep first
+qualify row_number() OVER(
+    PARTITION BY vendor_id, pickup_datetime, pickup_location_id, service_type
+    ORDER BY dropoff_datetime
+) = 1
--- module-4-dbt/taxi_rides_ny/models/intermediate/int_trips_unioned.sql
+++ module-4-dbt/taxi_rides_ny/models/intermediate/int_trips_unioned.sql
@@ -3,11 +3,17 @@
     Demonstrates how to combine data from multiple sources with slightly different schemas
 #}
 
+{{
+    config(
+        materialized='view'
+    )
+}}
+
 WITH green_tripdata AS (
     SELECT
         -- exclude loading information (unique_row_id, filename)
-        vendor_id
-        rate_code_id
+        vendor_id,
+        rate_code_id,
         pickup_location_id,
         dropoff_location_id,
         pickup_datetime,
@@ -25,11 +31,11 @@ WITH green_tripdata AS (
         payment_type,
         congestion_surcharge,
         total_amount,
-        'Green' as taxi_type
+        'Green' as service_type
     from {{ ref('stg_green_tripdata') }} 
 ),
 
-yellow_trips AS (
+yellow_tripdata AS (
     SELECT
         -- exclude loading information (unique_row_id, filename)
         vendor_id,
@@ -41,7 +47,7 @@ yellow_trips AS (
         store_and_fwd_flag,
         passenger_count,
         trip_distance,
-        cast(1 as integer) as trip_type,  -- Yellow taxis only do street-hail (code 1)
+        CAST(1 AS INTEGER) AS trip_type,  -- Yellow taxis only do street-hail (code 1)
         fare_amount,
         extra,
         mta_tax,
@@ -49,11 +55,12 @@ yellow_trips AS (
         tolls_amount,
         improvement_surcharge,
         payment_type,
+        congestion_surcharge,
         total_amount,
         'Yellow' as service_type
     FROM {{ ref('stg_yellow_tripdata') }}
 )
 
-SELECT * FROM green_trips
+SELECT * FROM green_tripdata
 UNION ALL
-SELECT * FROM yellow_trips
+SELECT * FROM yellow_tripdata
