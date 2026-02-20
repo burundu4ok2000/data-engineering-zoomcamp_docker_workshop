538b4c913f7976a2c978967fbc5dbb8998596bc5
Stan_Buren <burundu4ok2000@gmail.com>
2026-02-20 09:55:10 +0000

    Completed /staging dbt directory

--- module-4-dbt/taxi_rides_ny/models/staging/stg_green_tripdata.sql
+++ module-4-dbt/taxi_rides_ny/models/staging/stg_green_tripdata.sql
@@ -1,16 +1,20 @@
 {{ config(materialized='view') }}
 
 WITH sourse AS (
-    SELECT * FROM {{ source ('raw', 'green_tripdata') }}
+    SELECT * FROM {{ source ('raw', 'green_tripdata') }} -- This table comes from dbt_project.yml
 ),
 
 renamed AS (
     SELECT
+        -- loading information
+        CAST(unique_row_id AS BYTES) AS unique_row_id
+        CAST(filename AS STRING) AS file_name
+
         -- identifiers
         CAST(VendorID AS INTEGER) AS vendor_id
-        {{ safecast('RatecodeID', 'INTEGER' ) }} as rate_code_id,
-        CAST(PULocationID AS INTEGER) AS pickup_location_id,
-        CAST(DOLocationID AS INTEGER) AS dropoff_location_id,
+        {{ safecast('RatecodeID', 'INTEGER') }} as rate_code_id,
+        {{ safecast('PULocationID', 'INTEGER') }} AS pickup_location_id,
+        {{ safecast('DOLocationID', 'INTEGER') }} AS dropoff_location_id,
 
         -- timestamps
         CAST(lpep_pickup_datetime AS TIMESTAMP) AS pickup_datetime, 
@@ -28,11 +32,17 @@ renamed AS (
         CAST(mta_tax AS NUMERIC) AS mta_tax,
         CAST(tip_ammount AS NUMERIC) AS tip_ammount,
         CAST(tolls_amount AS NUMERIC) AS tolls_amount,
-        --CAST(ehail_fee AS NUMERIC) AS ehail_fee, Column provides no analytical value and is typically excluded from downstream models.
-        CAST(payment_type AS INTEGER) AS payment_type -- Standard DE Zoomcamp course ignored this column, but I'm including it for tip-ratio analysis
+        -- CAST(ehail_fee AS NUMERIC) AS ehail_fee, Column provides no analytical value and is typically excluded from downstream models.
         CAST(improvement_surcharge AS NUMERIC) AS improvement_surcharge,
         CAST(total_amount AS NUMERIC) AS total_amount,
         {{ safecast('payment_type','INTEGER') }} AS payment_type
+        CAST(congestion_surcharge AS NUMERIC) AS congestion_surcharge -- Standard DE Zoomcamp course ignored this column, but I'm including it
     FROM source
-    WHERE vendorid IS NOT NULL
+    WHERE vendorid IS NOT NULL -- filter all 12.45% of "Dispatch" records originating from HVFHV bases
 )
+
+SELECT * FROM renamed
+
+{% if target.name == 'dev' %}
+    WHERE pickup_datetime >= '2019-01-01' and pickup_datetime < '2019-02-01'
+{$ endif %}
